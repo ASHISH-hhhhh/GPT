3bd4433a833fe235d8b3081b7e3ce062a7da3191
ashish <ashishbiradar060@gmail.com>
2026-09-01 19:47:51 +0530

    Fixed the wrong function usage for token Increment.Implemented token rate Limiter but not Tested yet

--- services/incrTokensConsumed.js
+++ services/incrTokensConsumed.js
@@ -1,6 +1,6 @@
 import { redisClient } from "../config/redisConnect.js";
 
 const incrTokensConsumed = async (key, tokenValue) => {
-  await redisClient.incr(key, tokenValue);
+  await redisClient.incrBy(key, tokenValue);
 };
 export default incrTokensConsumed;
